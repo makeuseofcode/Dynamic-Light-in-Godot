659ab3a30af8146fda6878bbd1e6ad4f6455643e
Imran Alam <75203259+Imranalam28@users.noreply.github.com>
2023-08-08 07:18:29 +0530

    Create simple-movement.gd

---
+++ simple-movement.gd
@@ -0,0 +1,14 @@
+var speed = 200
+
+func _physics_process(delta):
+    var velocity = Vector2()
+    if Input.is_action_pressed('ui_right'):
+        velocity.x += 1
+    if Input.is_action_pressed('ui_left'):
+        velocity.x -= 1
+    if Input.is_action_pressed('ui_down'):
+        velocity.y += 1
+    if Input.is_action_pressed('ui_up'):
+        velocity.y -= 1
+    velocity = velocity.normalized() * speed
+    move_and_collide(velocity * delta)
